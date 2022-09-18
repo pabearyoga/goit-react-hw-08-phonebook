3e814d4954539bc10756b31cf4bf6d182d0a0fe3
Pabearyoga <pabearyoga@gmail.com>
2022-09-18 20:25:35 +0300

    fix ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,10 +5,23 @@ import css from "./ContactList.module.css"
 
 const ContactList  = ({contacts, onDelete }) => (
 
-            <ul className={css.contactList}>
-                {contacts.map(contact => (<li key={contact.id} className={css.contactItem}>{contact.name}: {contact.number} <button type="button" className={css.contactBtn} onClick={() => onDelete(contact.id)}>x</button></li>))}
-            </ul>
-        );
+  <ul className={css.contactList}>
+    {contacts.map(contact => (
+      <li
+        key={contact.id}
+        className={css.contactItem}
+      >
+        {contact.name}: {contact.number}
+        <button
+          type="button"
+          className={css.contactBtn}
+          onClick={() => onDelete(contact.id)}
+        >
+          x
+        </button>
+      </li>))}
+  </ul>
+  );
 
 export default ContactList
 
