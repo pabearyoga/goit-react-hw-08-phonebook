82287052f1554f143b480d200000c0f129a4178c
Pabearyoga <pabearyoga@gmail.com>
2022-09-18 13:05:46 +0300

    fix label id

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import css from "./Filter.module.css"
+import { nanoid } from "nanoid"
 
-const Filter = ({value, onChange}) => (
-            <label className={css.filterLabel}>Find contacts by name:
+const loginInputId = nanoid();
+
+
+const Filter = ({ value, onChange }) => (
+    
+            <label htmlFor={loginInputId} className={css.filterLabel}>Find contacts by name:
                 <input
                     type="text"
                     className={css.inputFilter}
                     name="filter"
+                    id={loginInputId}
                     value={value}
                     onChange={onChange}
                     required
