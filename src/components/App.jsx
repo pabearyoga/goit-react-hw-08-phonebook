2f908ad3a08a6e72af72bf350582c53a4894b1f6
Pabearyoga <pabearyoga@gmail.com>
2022-09-28 16:30:48 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
 import Filter from "./Filter/Filter";
