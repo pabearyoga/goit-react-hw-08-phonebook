9747990dd038c5baa2462d63cd257a55cef7d192
Pabearyoga <pabearyoga@gmail.com>
2022-10-14 22:36:04 +0300

    add Contacts amount

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,14 +1,20 @@
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
 import { StatusFilter } from 'components/ContactsFilter/ContactsFilter.js';
 import css from './AppBar.module.css';
 
 export const AppBar = () => {
+  const contacts = useSelector(getContacts);
+
   return (
     <header className={css.wrapper}>
       <section className={css.section}>
-        <h2 className={css.title}>Phonebook</h2>
+        <h2 className={css.title}>
+          Contacts: <span>{contacts.length}</span>
+        </h2>
       </section>
       <section className={css.section}>
-        <h2 className={css.title}>Filter by status</h2>
+        <h2 className={css.title}>Find contacts by name:</h2>
         <StatusFilter />
       </section>
     </header>
