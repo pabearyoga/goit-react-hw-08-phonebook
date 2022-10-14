309c747872bc1d1b068a80a3e8631670f551c3f8
Pabearyoga <pabearyoga@gmail.com>
2022-10-14 22:24:18 +0300

    add alert if name already in contacts

---
+++ src/components/ContactsFilter/ContactsFilter.js
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { setFilter } from 'redux/filtersSlice';
+import css from './ContactsFilter.module.css';
+
+export const StatusFilter = () => {
+  const dispatch = useDispatch();
+
+  const handleFilterChange = event => {
+    const filterInput = event.target;
+    dispatch(setFilter(filterInput.value));
+  };
+
+  return (
+    <div className={css.wrapper}>
+      <input
+        type="text"
+        className={css.inputFilter}
+        name="filter"
+        // value={value}
+        onChange={handleFilterChange}
+        required
+      />
+    </div>
+  );
+};
