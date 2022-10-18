6c4f01271255840143a1817baffaa147c12ed774
Pabearyoga <pabearyoga@gmail.com>
2022-10-18 16:05:03 +0300

    fix cotnact tel style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
-import { MdClose } from 'react-icons/md';
+import { MdClose, MdOutlinePhoneIphone } from 'react-icons/md';
 import { deleteContact } from 'redux/contactsSlice';
 import css from './Contact.module.css';
 
@@ -8,14 +8,23 @@ export const Contact = ({ contact }) => {
 
   const handleDelete = () => dispatch(deleteContact(contact.id));
 
+  const tel = contact.number;
   return (
     <div
       className={`${css.wrapper} ${css.animate__animated} ${css.animate__bounceInUp}`}
     >
       <div className={css.textWrapper}>
-        <p className={css.text}>{contact.name}:</p>
-        <p className={css.text}>{contact.number}</p>
+        <p className={css.text}>
+          {contact.name}: {contact.number}
+        </p>
+        {/* <p className={css.text}>{contact.number}</p> */}
       </div>
+      <button className={css.btn} onClick={handleDelete}>
+        <a href={`tel:${tel}`}>
+          <MdOutlinePhoneIphone fill="#4169e1" size={20} />
+        </a>
+      </button>
+
       <button className={css.btn} onClick={handleDelete}>
         <MdClose size={24} />
       </button>
