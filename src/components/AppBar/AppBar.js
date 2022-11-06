6cfb5c9f76a69f15bea2637381ee43d7d5483ba7
Pabearyoga <pabearyoga@gmail.com>
2022-11-06 14:29:00 +0200

    fix some style

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,16 +1,11 @@
-import { Navigation } from '../Navigation/Navigation';
-import { UserMenu } from '../UserMenu/UserMenu';
-import { AuthNav } from '../AuthNav/AuthNav';
-import { useAuth } from '../../hooks/useAuth';
 import css from './AppBar.module.css';
 
 export const AppBar = () => {
-  const { isLoggedIn } = useAuth();
-
   return (
-    <header className={css.header}>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    <header className={css.wrapper}>
+      <section className={css.section}>
+        <h2 className={css.logo}>Phonebook</h2>
+      </section>
     </header>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { tasksReducer } from './tasks/slice';
-import { authReducer } from './auth/slice';
-
-// Persisting token field from auth slice to localstorage
-const authPersistConfig = {
-  key: 'auth',
-  storage,
-  whitelist: ['token'],
-};
+import { contactsReducer } from './contactsSlice';
+import { filtersReducer } from './filtersSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: persistReducer(authPersistConfig, authReducer),
-    tasks: tasksReducer,
+    contacts: contactsReducer,
+    filters: filtersReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
