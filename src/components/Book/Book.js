b269dc2b3dfcb7066c6e62c505c25829265d7d1f
Pabearyoga <pabearyoga@gmail.com>
2023-08-01 01:16:58 +0300

    fix Book component

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -6,23 +6,10 @@ export const Book = () => {
     <div className={css.book}>
       <div className={css.book__pgShadow}></div>
       <div className={css.book__pg}></div>
-      <div
-        // class="book__pg book__pg--2"
-        className={`${css.book__pg} ${css.book__pg2}`}
-      ></div>
-      <div
-        // class="book__pg book__pg--3"
-        className={`${css.book__pg} ${css.book__pg3}`}
-      ></div>
-      <div
-        // class="book__pg book__pg--4"
-        className={`${css.book__pg} ${css.book__pg4}`}
-      ></div>
-      <div
-        // class="book__pg book__pg--5"
-        className={`${css.book__pg} ${css.book__pg5}`}
-        // className={`${css.wrapper} ${css.animate__animated} ${css.animate__bounceInUp}`}
-      ></div>
+      <div className={`${css.book__pg} ${css.book__pg2}`}></div>
+      <div className={`${css.book__pg} ${css.book__pg3}`}></div>
+      <div className={`${css.book__pg} ${css.book__pg4}`}></div>
+      <div className={`${css.book__pg} ${css.book__pg5}`}></div>
     </div>
   );
 };
